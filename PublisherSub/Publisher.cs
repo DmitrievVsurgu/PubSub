2bc598ccdd46c282e6a5359ba2200db664decd6b
Dmitriev Vladimir <Dmitriev.V.G11@yandex.ru>
2017-12-26 12:50:12 +0500

    create pubsub

---
+++ PublisherSub/Publisher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using ZeroMQ;
+
+namespace Examples
+{
+    class Publisher
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new ZContext())
+            using (var publisher = new ZSocket(context, ZSocketType.PUB))
+            {
+                //publisher.Linger = TimeSpan.Zero;
+                publisher.Bind("tcp://*:5555");
+
+                int published = 0;
+                while (true)
+                {
+
+                    using (var message = new ZMessage())
+                    {
+                        published++;
+                        message.Add(new ZFrame(string.Format("B {0}", published)));
+                        message.Add(new ZFrame(string.Format("Still to New Year {0} days", (new DateTime(DateTime.Now.Year + 1, 01, 01) - DateTime.Now).Days.ToString())));
+                        Thread.Sleep(10000);
+
+                        Console.WriteLine("Publishing" + " the message is sent");
+                        publisher.Send(message);
+                    }
+
+                    using (var message = new ZMessage())
+                    {
+                        published++;
+                        message.Add(new ZFrame(string.Format("A {0}", published)));
+                        message.Add(new ZFrame(string.Format("Heppy New Year!!!")));
+                        Thread.Sleep(10000);
+
+                        Console.WriteLine("Publishing " + "the message is sent");
+                        publisher.Send(message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SubscriberB/SubscriberB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using ZeroMQ;
+
+namespace Examples
+{
+    class SubscriberB
+    {
+        public static void Main(string[] args)
+        {
+            using (var context = new ZContext())
+            using (var subscriber = new ZSocket(context, ZSocketType.SUB))
+            {
+                subscriber.Connect("tcp://127.0.0.1:5555");
+                subscriber.Subscribe("B");
+
+                int subscribed = 0;
+                while (true)
+                {
+                    using (ZMessage message = subscriber.ReceiveMessage())
+                    {
+                        subscribed++;
+
+                        // Read envelope with address
+                        string address = message[0].ReadString();
+
+                        // Read message contents
+                        string contents = message[1].ReadString();
+
+                        Console.WriteLine("{0}. [{1}] {2}", subscribed, address, contents);
+                    }
+                }
+            }
+        }
+    }
+}
+
